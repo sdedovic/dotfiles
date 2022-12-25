4854ea45878ccec5043d8c90867e317f995c2119
stevan <stevan@dedovic.com>
2022-12-24 21:38:49 -0600

    add language configs
    add which-key

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -11,6 +11,7 @@ return require('packer').startup(function(use)
   use 'NLKNguyen/papercolor-theme'
   use 'sainnhe/gruvbox-material'
 
+  -- Telescope
 	use { 
 		'nvim-telescope/telescope.nvim', tag = '0.1.0', 
 		requires = { 
@@ -22,8 +23,17 @@ return require('packer').startup(function(use)
 		} 
 	}
 
+  use {
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').setup {}
+    end
+  }
+
+  -- Language Specific
   use 'pangloss/vim-javascript'
   use 'maxmellon/vim-jsx-pretty'
-
+  use 'tpope/vim-fireplace'
   use 'LnL7/vim-nix'
+  use 'hashivim/vim-terraform'
 end)

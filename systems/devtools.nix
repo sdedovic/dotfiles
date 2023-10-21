ccd6bbd8d652d70b44222ddefbc15dfb119383d0
sdedovic <stevan@dedovic.com>
2023-10-21 22:40:31 +0000

    Revert "remove configs due to uknown bug"
    This reverts commit 890d3fa8641fa4ce57d249960e1c6f50d510cffa.

--- systems/devtools.nix
+++ systems/devtools.nix
@@ -1,10 +1,23 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    neovim
-    alacritty
-    tmux
-    ripgrep
-    direnv
-    jq
-  ];
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.dotfiles.devtools;
+in {
+  imports = [];
+  options = {
+    enable = lib.mkEnableOption "devtools";
+  };
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      neovim
+      alacritty
+      tmux
+      ripgrep
+      direnv
+      jq
+    ];
+  };
 }

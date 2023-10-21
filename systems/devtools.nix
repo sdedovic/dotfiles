890d3fa8641fa4ce57d249960e1c6f50d510cffa
sdedovic <stevan@dedovic.com>
2023-10-21 22:34:27 +0000

    remove configs due to uknown bug

--- systems/devtools.nix
+++ systems/devtools.nix
@@ -1,23 +1,10 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.dotfiles.devtools;
-in {
-  imports = [];
-  options = {
-    enable = lib.mkEnableOption "devtools";
-  };
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      neovim
-      alacritty
-      tmux
-      ripgrep
-      direnv
-      jq
-    ];
-  };
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    neovim
+    alacritty
+    tmux
+    ripgrep
+    direnv
+    jq
+  ];
 }

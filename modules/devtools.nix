45f000575af46b0febe7bb42232b33313c264461
sdedovic <stevan@dedovic.com>
2023-10-22 00:35:00 +0000

    fix zsh

--- modules/devtools.nix
+++ modules/devtools.nix
@@ -32,32 +32,6 @@ in {
       };
       syntaxHighlighting.enable = true;
       initExtra = ''
-        # direnv python
-        setopt PROMPT_SUBST
-        show_virtual_env() {
-          if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-              echo "($(basename $VIRTUAL_ENV))"
-          fi
-        }
-        PS1='$(show_virtual_env)'$PS1
-
-        # sdkman
-        export SDKMAN_DIR="${HOME}/.sdkman"
-        [[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
-
-        # nvim
-        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-        # rvim
-        export PATH="$PATH:$HOME/.rvm/bin"
-
-        # cargo
-        if [ -e $HOME/.cargo/bin ] && [ -d $HOME/.cargo/bin ]; then export PATH=$HOME/.cargo/bin:$PATH; fi
-
-
-        # prompt stuff
         if [[ -n "$DISPLAY" && -z "$TMUX" ]];
         then
           exec tmux new-session;

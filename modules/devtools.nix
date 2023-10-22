9279257f122e90b435b6e5118b03de70f1b077fb
sdedovic <stevan@dedovic.com>
2023-10-22 07:45:30 +0000

    bug: ripgrep path 2

--- modules/devtools.nix
+++ modules/devtools.nix
@@ -17,6 +17,7 @@ in {
     programs.neovim = {
       enable = true;
       defaultEditor = true;
+      extraPackages = [ pkgs.ripgrep ];
       plugins = with pkgs.vimPlugins; [
         # lsp
         {

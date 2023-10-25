27e6e06c468b863329bb6269a523162521012b39
sdedovic <stevan@dedovic.com>
2023-10-25 23:08:36 +0000

    add: mpd
    add: ncmpcpp

--- modules/awesome/default.nix
+++ modules/awesome/default.nix
@@ -48,6 +48,70 @@ in {
       '';
     };
 
+    programs.ncmpcpp = {
+      enable = true;
+      bindings = [
+        {
+          key = "j";
+          command = "scroll_down";
+        }
+        {
+          key = "k";
+          command = "scroll_up";
+        }
+        {
+          key = "J";
+          command = "page_down";
+        }
+        {
+          key = "K";
+          command = "page_up";
+        }
+        {
+          key = "h";
+          command = "previous_column";
+        }
+        {
+          key = "l";
+          command = "next_column";
+        }
+      ];
+      settings = {
+        mpd_host = "127.0.0.1";
+        mpd_port = "6600";
+
+        song_columns_list_format = "(7f)[green]{l} (25)[cyan]{a} (3f)[white]{n} (40)[]{t|f} (30)[red]{b}";
+
+        display_bitrate = "yes";
+        user_interface = "alternative";
+        startup_screen = "media_library";
+        main_window_color = "white";
+
+        media_library_primary_tag = "album_artist";
+        media_library_albums_split_by_date = "no";
+
+        current_item_inactive_column_prefix = "$(250)$r";
+        current_item_inactive_column_suffix = "$/r$(end)";
+        current_item_prefix = "$(2)$r";
+        current_item_suffix = "$/r$(end)";
+
+        visualizer_fifo_path = "/tmp/mpd.fifo";
+        visualizer_output_name = "my_fifo";
+        visualizer_sync_interval = 30;
+        visualizer_look = "██";
+        visualizer_color = "136,135,134,133,132,131";
+        visualizer_type = "spectrum";
+        visualizer_spectrum_smooth_look = "yes";
+        visualizer_in_stereo = "yes";
+        visualizer_spectrum_hz_max = 16000;
+        visualizer_spectrum_gain = 26;
+
+        progressbar_look = "██-";
+        progressbar_color = "white";
+        progressbar_elapsed_color = "green";
+      };
+    };
+
     programs.alacritty = {
       enable = true;
       settings = {

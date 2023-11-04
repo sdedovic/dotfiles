292b11513ef47b6f036dca90b3abe54ff0782841
sdedovic <stevan@dedovic.com>
2023-11-04 02:35:11 +0000

    change: fix dl-mp4 script

--- modules/dl-mp4.sh
+++ modules/dl-mp4.sh
@@ -1,12 +1,10 @@
 dl-mp4 () {
-  ext="$(echo $1 | rev | cut -d"." -f1 | rev)"
-  tmp_name="${original}.${ext}"
   new_name="${2:-output.mp4}"
   folder="$(mktemp -d)"
   crf="${3:-28}"
 
-  yt-dlp $1 -P "${folder}/" -o ${tmp_name}
-  ffmpeg -i "${folder}/${tmp_name}" -vcodec libx264 -crf ${crf} -pix_fmt yuv420p ${new_name} -y
+  tmp_name="$(yt-dlp $1 -P "${folder}/" -o ${tmp_name} --print after_move:filepath)"
+  ffmpeg -i "${tmp_name}" -vcodec libx264 -crf ${crf} -pix_fmt yuv420p ${new_name} -y
 
   echo "\n$1 -> ${new_name}\n"
 }

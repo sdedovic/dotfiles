3e7f27b9407cd1d9148f6ad77c101a0bb94d23a1
stevan <stevan@dedovic.com>
2024-01-03 00:40:55 -0600

    add home-manager for non-nix systems

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     flake-utils,
     nixpkgs,
     home-manager,
-  }:
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
@@ -23,12 +23,48 @@
       };
     in {
       formatter = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          home-manager.packages.${system}.home-manager
+        ];
+      };
     })
-    // {
+    // (let
+      homeManagerSetup = homeDirectory: username: (
+        let
+          specialArgs =
+            inputs
+            // {
+              inherit homeDirectory;
+              isNixOS = false;
+            };
+        in
+          home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs {
+              # TODO: move system to homeManagerSetup arg and maybe replace the inputs with an attribute set
+              system = "x86_64-linux";
+              config.allowUnfree = true;
+            };
+            modules = [
+              ./modules/devtools.nix
+              {
+                home.devtools.enable = true;
+                home.stateVersion = "23.11";
+                home.username = username;
+                home.homeDirectory = homeDirectory;
+              }
+            ];
+            extraSpecialArgs = specialArgs;
+          }
+      );
+    in {
+      homeConfigurations = {
+        stevan-wsl = homeManagerSetup "/root" "root";
+      };
       homeManagerModules = {
         devtools = {...}: {
           imports = [./modules/devtools.nix];
         };
       };
-    };
+    });
 }

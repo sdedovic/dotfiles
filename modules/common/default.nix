462b6e4044ef996d94f6a37192a92594c190eeb5
sdedovic <stevan@dedovic.com>
2025-01-23 05:35:18 +0000

    add zellij

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -12,6 +12,16 @@
   ];
 
   config = {
+    # TODO(2025-01-22): move to own file / dir
+    programs.zellij = {
+      enable = true;
+
+      # not sure if still supported and
+      #  this will add some zsh init stuff I dont want yet
+      #
+      # enableZshIntegration = true;
+    };
+
     home.packages = with pkgs; [
       # general system utilities
       ripgrep
